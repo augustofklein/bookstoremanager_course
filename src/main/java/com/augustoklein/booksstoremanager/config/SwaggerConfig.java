b0e5c3faf83d10917c514d3bbb8fa0e7648e2c55
Augusto Fernando Klein <augusto@MacBook-Air-de-Augusto.local>
2022-09-18 22:00:11 -0300

    Add swagger configuration

---
+++ src/main/java/com/augustoklein/booksstoremanager/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.augustoklein.booksstoremanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private static final String BASE_PACKAGE = "com.augustoklein.booksstoremanager";
+    private static final String API_TITLE = "Bookstore Manager Course";
+    private static final String API_DESCRIPTION = "Bookstore Manager API Professional";
+    private static final String API_VERSION = "1.0.0";
+    private static final String CONTACT_NAME = "Augusto Fernando Klein";
+    private static final String CONTACT_GITHUB = "https://github.com/augustofklein";
+    private static final String CONTACT_EMAIL = "augusto_klein@teste.com";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version(API_VERSION)
+                .contact(new Contact(CONTACT_NAME, CONTACT_GITHUB, CONTACT_EMAIL))
+                .build();
+    }
+
+}
